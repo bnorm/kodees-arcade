43ae88de00c06e19f8b9a67cf6b12b4bca37988c
Brian Norman <brian@bnorm.com>
2026-07-09 07:08:24 -0500

    Create race and track view models

---
+++ arcade-machine/arcade-display/src/commonMain/kotlin/dev/bnorm/arcade/display/ViewModel.kt
@@ -0,0 +1,32 @@
+package dev.bnorm.arcade.display
+
+import androidx.compose.runtime.Composable
+import app.cash.molecule.RecompositionMode
+import app.cash.molecule.launchMolecule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
+abstract class ViewModel<Event, Model>(
+    private val scope: CoroutineScope
+) {
+    // Events have a capacity large enough to handle simultaneous UI events,
+    // but small enough to surface issues if they get backed up for some reason.
+    private val events = MutableSharedFlow<Event>(extraBufferCapacity = 20)
+
+    val models: StateFlow<Model> by lazy(LazyThreadSafetyMode.NONE) {
+        scope.launchMolecule(mode = RecompositionMode.ContextClock) {
+            models(events)
+        }
+    }
+
+    protected fun take(event: Event) {
+        if (!events.tryEmit(event)) {
+            error("Event buffer overflow.")
+        }
+    }
+
+    @Composable
+    protected abstract fun models(events: Flow<Event>): Model
+}
