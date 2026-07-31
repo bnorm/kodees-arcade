fb3cac279b71ef48673b88bc2fa1344fe727ea8a
Brian Norman <brian@bnorm.com>
2026-07-30 20:33:40 -0500

    fixup! Add icons to reorder drivers

---
+++ arcade-machine/arcade-display/src/commonMain/kotlin/dev/bnorm/arcade/display/asset/icon/keyboard_arrow_up.kt
@@ -0,0 +1,51 @@
+package dev.bnorm.arcade.display.asset.icon
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val keyboard_arrow_up: ImageVector
+    get() {
+        if (_keyboard_arrow_up != null) {
+            return _keyboard_arrow_up!!
+        }
+        _keyboard_arrow_up =
+            ImageVector.Builder(
+                name = "keyboard_arrow_up",
+                defaultWidth = 24.dp,
+                defaultHeight = 24.dp,
+                viewportWidth = 24f,
+                viewportHeight = 24f,
+            )
+                .apply {
+                    path(
+                        fill = SolidColor(Color.Black),
+                        fillAlpha = 1f,
+                        stroke = null,
+                        strokeAlpha = 1f,
+                        strokeLineWidth = 1f,
+                        strokeLineCap = StrokeCap.Butt,
+                        strokeLineJoin = StrokeJoin.Bevel,
+                        strokeLineMiter = 1f,
+                        pathFillType = PathFillType.NonZero,
+                    ) {
+                        moveTo(12f, 10.8f)
+                        lineTo(7.4f, 15.4f)
+                        lineTo(6f, 14f)
+                        lineTo(12f, 8f)
+                        lineToRelative(6f, 6f)
+                        lineToRelative(-1.4f, 1.4f)
+                        lineTo(12f, 10.8f)
+                        close()
+                    }
+                }
+                .build()
+        return _keyboard_arrow_up!!
+    }
+
+private var _keyboard_arrow_up: ImageVector? = null
