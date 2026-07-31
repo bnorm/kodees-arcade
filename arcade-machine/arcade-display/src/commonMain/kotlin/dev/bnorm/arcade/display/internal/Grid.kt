043f66af878d3dc8df772fcde6f302172917e7fe
Brian Norman <brian@bnorm.com>
2026-07-31 13:51:56 -0500

    Add scrollbars to grid display

--- arcade-machine/arcade-display/src/commonMain/kotlin/dev/bnorm/arcade/display/internal/Grid.kt
+++ arcade-machine/arcade-display/src/commonMain/kotlin/dev/bnorm/arcade/display/internal/Grid.kt
@@ -1,13 +1,20 @@
 package dev.bnorm.arcade.display.internal
 
+import androidx.compose.foundation.HorizontalScrollbar
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.v2.ScrollbarAdapter
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Rect
@@ -24,67 +31,82 @@ import kotlin.math.roundToInt
 fun Grid(
     rows: Int,
     columns: Int,
-    modifier: Modifier = Modifier.Companion,
+    modifier: Modifier = Modifier,
     stickyRows: Int = 0,
     stickyColumns: Int = 0,
     content: @Composable () -> Unit
 ) {
     val vertical = remember { ResultsScrollState() }
     val horizontal = remember { ResultsScrollState() }
-    Layout(
-        content = content,
-        modifier = modifier
-            .scrollable(vertical.scrollableState, Orientation.Vertical)
-            .scrollable(horizontal.scrollableState, Orientation.Horizontal)
-            .clip(GridShape) // Clip start and top.
-    ) { measurables, constraints ->
-        require(measurables.size == columns * rows)
+    Box(modifier = modifier) {
+        Layout(
+            content = content,
+            modifier = Modifier
+                .scrollable(vertical.scrollableState, Orientation.Vertical)
+                .scrollable(horizontal.scrollableState, Orientation.Horizontal)
+                .clip(GridShape) // Clip start and top.
+        ) { measurables, constraints ->
+            require(measurables.size == columns * rows)
 
-        val intrinsicHeights = measurables.map { it.maxIntrinsicHeight(Int.MAX_VALUE) }
-        val intrinsicWidths = measurables.map { it.maxIntrinsicWidth(Int.MAX_VALUE) }
+            val intrinsicHeights = measurables.map { it.maxIntrinsicHeight(Int.MAX_VALUE) }
+            val intrinsicWidths = measurables.map { it.maxIntrinsicWidth(Int.MAX_VALUE) }
 
-        val rowHeights = IntArray(rows) { r -> (0..<columns).maxOf { c -> intrinsicHeights[r * columns + c] } }
-        val columnsWidths = IntArray(columns) { c -> (0..<rows).maxOf { r -> intrinsicWidths[r * columns + c] } }
-        val placeables = Array(rows) { r ->
-            val height = rowHeights[r]
-            Array(columns) { c ->
-                val width = columnsWidths[c]
-                measurables[r * columns + c].measure(Constraints.fixed(width, height))
+            val rowHeights = IntArray(rows) { r -> (0..<columns).maxOf { c -> intrinsicHeights[r * columns + c] } }
+            val columnsWidths = IntArray(columns) { c -> (0..<rows).maxOf { r -> intrinsicWidths[r * columns + c] } }
+            val placeables = Array(rows) { r ->
+                val height = rowHeights[r]
+                Array(columns) { c ->
+                    val width = columnsWidths[c]
+                    measurables[r * columns + c].measure(Constraints.fixed(width, height))
+                }
+            }
+
+            val totalHeight = rowHeights.sum()
+            val layoutHeight = minOf(totalHeight, constraints.maxHeight)
+            vertical.updateBounds(totalHeight, layoutHeight)
+
+            val totalWidth = columnsWidths.sum()
+            val layoutWidth = minOf(totalWidth, constraints.maxWidth)
+            horizontal.updateBounds(totalWidth, layoutWidth)
+
+            layout(layoutWidth, layoutHeight) {
+                val vOffset = -vertical.scrollOffset.roundToInt()
+                val hOffset = -horizontal.scrollOffset.roundToInt()
+
+                var yOffset = 0
+                for (r in 0..<rows) {
+                    var xOffset = 0
+
+                    val placeableRow = placeables[r]
+                    val stickyRow = r < stickyRows
+                    val y = yOffset + if (stickyRow) 0 else vOffset
+
+                    for (c in 0..<columns) {
+                        val placeable = placeableRow[c]
+                        val stickyColumn = c < stickyColumns
+                        val x = xOffset + if (stickyColumn) 0 else hOffset
+
+                        val zIndex = (if (stickyRow) 1f else 0f) + (if (stickyColumn) 1f else 0f)
+                        placeable.placeRelative(x, y, zIndex)
+
+                        xOffset += columnsWidths[c]
+                    }
+                    yOffset += rowHeights[r]
+                }
             }
         }
 
-        val totalHeight = rowHeights.sum()
-        val layoutHeight = minOf(totalHeight, constraints.maxHeight)
-        vertical.updateBounds((totalHeight - layoutHeight).coerceAtLeast(0))
-
-        val totalWidth = columnsWidths.sum()
-        val layoutWidth = minOf(totalWidth, constraints.maxWidth)
-        horizontal.updateBounds((totalWidth - layoutWidth).coerceAtLeast(0))
-
-        layout(layoutWidth, layoutHeight) {
-            val vOffset = -vertical.value.roundToInt()
-            val hOffset = -horizontal.value.roundToInt()
-
-            var yOffset = 0
-            for (r in 0..<rows) {
-                var xOffset = 0
-
-                val placeableRow = placeables[r]
-                val stickyRow = r < stickyRows
-                val y = yOffset + if (stickyRow) 0 else vOffset
-
-                for (c in 0..<columns) {
-                    val placeable = placeableRow[c]
-                    val stickyColumn = c < stickyColumns
-                    val x = xOffset + if (stickyColumn) 0 else hOffset
-
-                    val zIndex = (if (stickyRow) 1f else 0f) + (if (stickyColumn) 1f else 0f)
-                    placeable.placeRelative(x, y, zIndex)
-
-                    xOffset += columnsWidths[c]
-                }
-                yOffset += rowHeights[r]
-            }
+        Box(modifier = Modifier.matchParentSize()) {
+            VerticalScrollbar(
+                adapter = vertical,
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+            )
+            HorizontalScrollbar(
+                adapter = horizontal,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+            )
         }
     }
 }
@@ -112,23 +134,36 @@ private object GridShape : Shape {
     }
 }
 
-private class ResultsScrollState {
-    var value by mutableFloatStateOf(0f)
+private class ResultsScrollState : ScrollbarAdapter {
+    override var scrollOffset by mutableDoubleStateOf(0.0)
         private set
 
-    private var maxScrollBounds by mutableFloatStateOf(0f)
+    override var contentSize by mutableDoubleStateOf(0.0)
+        private set
+    override var viewportSize by mutableDoubleStateOf(0.0)
+        private set
+
+    private val maxScrollBounds: Double
+        get() = (contentSize - viewportSize).coerceAtLeast(0.0)
 
     val scrollableState = ScrollableState { delta ->
-        val oldTarget = value
-        val newTarget = (value - delta).coerceIn(0f, maxScrollBounds)
-        value = newTarget
-        (oldTarget - newTarget)
+        val oldTarget = scrollOffset
+        val newTarget = (scrollOffset - delta).coerceIn(0.0, maxScrollBounds)
+        scrollOffset = newTarget
+        (oldTarget - newTarget).toFloat()
     }
 
-    fun updateBounds(maxBounds: Int) {
-        maxScrollBounds = maxBounds.toFloat()
-        if (value > maxScrollBounds) {
-            value = maxScrollBounds
+    override suspend fun scrollTo(scrollOffset: Double) {
+        scrollableState.scrollBy((this.scrollOffset - scrollOffset).toFloat())
+    }
+
+    fun updateBounds(contentSize: Int, viewportSize: Int) {
+        this.contentSize = contentSize.toDouble()
+        this.viewportSize = viewportSize.toDouble()
+
+        val maxScrollBounds = maxScrollBounds
+        if (scrollOffset > maxScrollBounds) {
+            scrollOffset = maxScrollBounds
         }
     }
 }
